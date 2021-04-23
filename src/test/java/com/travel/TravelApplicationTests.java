341b745bac6fe8db8c11c43a8aaa44bf8792d41d
anPetrichor <1198744851@qq.com>
2021-04-23 16:00:08 +0800

    travel

--- src/test/java/com/travel/TravelApplicationTests.java
+++ src/test/java/com/travel/TravelApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BysjApplicationTests {
+public class TravelApplicationTests {
 
     @Test
     public void contextLoads() {
