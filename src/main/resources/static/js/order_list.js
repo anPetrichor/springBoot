86e7542decd4ae9d65e54e546073c6eb34ac44ea
yuan <51240590+anPetrichor@users.noreply.github.com>
2021-04-25 13:01:03 +0800

    Delete src directory

--- src/main/resources/static/js/order_list.js
+++
@@ -1,7 +0,0 @@
-$("th,td").addClass("text-center");
-
-function changePageSize() {
-    var pageSize = $("#changePageSize").val();
-    location.href = "/manage/order/findAllOrder?page=1&size="
-        + pageSize;
-}
--- src/main/resources/static/js/user_detail.js
+++
@@ -1,2 +0,0 @@
-//表格中的th,td都设置为文本居中
-$("th,td").addClass("text-center");
